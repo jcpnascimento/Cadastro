e5d8c8177d9674efcad20c8a513392f1a4615123
jcpnascimento <jacksoncpn@outlook.com.br>
2024-03-01 15:36:50 -0300

    Validação de Dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
